1cf006222ce4882695bbb63efe342151c40afeaa
HastiaShashkova <Helelarchangel@mail.ru>
2024-05-07 09:39:55 +0700

    Initial commit

---
+++ прим2.cpp
@@ -0,0 +1,16 @@
+//пример 2 для гит
+#include <iostream>
+using namespace std;
+
+int main()
+{int x=27;
+int *ip;
+ip=&x;
+	cout<<"value of x is : ";
+		cout<<x<<endl;
+	cout<<"value of ip is : ";
+		cout<<ip<<endl;
+	cout<<"value of *ip is : ";
+		cout<<*ip<<endl;
+return 0;
+}
